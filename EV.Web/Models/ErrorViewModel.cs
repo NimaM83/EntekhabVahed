0738456a0cb600ae5cc8cf0bdea2216cc02fd0ce
bikaar <nima.268183@gmail.com>
2025-05-25 21:39:12 +0330

    Add project files.

---
+++ EV.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EV.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
